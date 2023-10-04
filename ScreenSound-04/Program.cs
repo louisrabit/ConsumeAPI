620fd3b4f27ee20f4d1eae87fb0152e4152aee92
Luis Manuel Brito Coelho <98318766+louisrabit@users.noreply.github.com>
2023-10-04 15:32:52 +0100

    Add project files.

---
+++ ScreenSound-04/Program.cs
@@ -0,0 +1,30 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+ string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+   
+}
+
+
+
+//using (HttpClient client = new HttpClient())
+//{
+//    try
+//    {
+//        string resposta = await client.GetStringAsync("https://anapioficeandfire.com/api/characters/583");
+//        Console.WriteLine(resposta);
+//    }
+
+
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"Temos um problema: {ex.Message}");
+//    }
+//}
